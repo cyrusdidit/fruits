15b4690357d87575ac99ba93615114f37a8d9a01
ipa23.p.ozolina <ipa23.p.ozolina@pjvt.local>
2024-12-10 11:04:41 +0200

    i wanna kms

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+
+//dump and die
+function dd($data){
+    echo "<pre>";
+    var_dump($data);
+    echo "</pre>";
+    die(); //stops code
+    };
+
+
+?>
--- index.php
+++ index.php
@@ -1,6 +1,49 @@
 <?php
 
+require "functions.php";
 
-echo "hi";
+echo "Rawr <br><br>";
 
+
+//connect db and php with PDO
+//datasource name
+$dsn = "mysql:host=localhost;port=3306;user=root;dbname=blogcyrus;charset=utf8mb4";
+//db object (php data object)
+$pdo = new PDO($dsn);
+
+//prepare statement
+
+$statement = $pdo->prepare("SELECT * FROM posts");
+
+//fillout statement
+$statement->execute();
+
+//get blog inputs
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC); //PDO::FETCH_ASSOC or just (2)
+//see it
+
+
+
+//foreach output each post in a table
+
+echo "<table border='1'>";
+echo "<tr><th>Content</th></tr>"; // Table header (only Content)
+
+// Loop through each post and output only the content
+foreach ($posts as $post) {
+    echo "<tr>";
+    echo "<td>" . ($post['content']) . "</td>"; // Output content
+    echo "</tr>";
+}
+
+echo "</table>";
+
+echo "<br>"; //not in a table below
+
+//list
+ echo "<ul>";
+foreach($posts as $post){
+    echo "<li>" . ($post['content']) . "</li>"; // Output content
+}
+echo "</ul>";
 ?>
